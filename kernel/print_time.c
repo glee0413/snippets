dad01ab501928bfb8825b2bfe35dcb32d8311eca
Gao Liang <liang.gao@reachauto.com>
2016-10-05 16:56:38 +0800

    添加内核中打印日期的函数

---
+++ kernel/print_time.c
@@ -0,0 +1,21 @@
+
+#include <linux/time.h>
+#include <linux/timex.h>
+#include <linux/rtc.h>
+
+void
+print_time(void){
+	struct timex  txc; 
+	struct rtc_time tm;
+
+	do_gettimeofday(&(txc.time));
+
+	txc.time.tv_sec -= sys_tz.tz_minuteswest * 60;
+	rtc_time_to_tm(txc.time.tv_sec,&tm);
+
+	printk("%04d-%02d-%02d %02d:%02d:%02d ",
+				tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,
+				tm.tm_hour,tm.tm_min,tm.tm_sec);
+
+}
+
