738c4f16826c9a8499ec462685c7f9016b7925ca
liang.gao <glee-0413@163.com>
2016-10-05 16:57:56 +0800

    去掉多余的空格

--- kernel/print_time.c
+++ kernel/print_time.c
@@ -14,8 +14,8 @@ print_time(void){
 	rtc_time_to_tm(txc.time.tv_sec,&tm);
 
 	printk("%04d-%02d-%02d %02d:%02d:%02d ",
-				tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,
-				tm.tm_hour,tm.tm_min,tm.tm_sec);
+		tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,
+		tm.tm_hour,tm.tm_min,tm.tm_sec);
 
 }
 
